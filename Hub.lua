ca837502a5b5e4e501e8e50e951af77cf4210b50
doanvantiennam2-wq <doanvantiennam2@gmail.com>
2026-03-27 17:33:42 +0700

    Add Hub.lua for game loading and script execution

---
+++ Hub.lua
@@ -0,0 +1,3 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().team = "Marines" -- Pirates or Marines
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vinh129150/hack/refs/heads/main/Bloxfruits.lua"))()
